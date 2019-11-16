c3ad970af8fd26d132bbc2e0db36307b4a62b3d3
Leticia Tiveron <leticiativeronbt@gmail.com>
2019-11-16 10:24:29 -0300

    Improve db connection

--- servidor-contato/src/model/database.js
+++ servidor-contato/src/model/database.js
@@ -1,13 +1,13 @@
-const mongoose = require('mongoose');
-mongoose.connect("mongodb://localhost:27017/reprograma", { useNewUrlParser: true });
-mongoose.set('useCreateIndex', true);
+const mongoose = require('mongoose')
+mongoose.connect("mongodb://localhost:27017/reprograma", { useNewUrlParser: true })
+mongoose.set('useCreateIndex', true)
 
 const connect = () => {
-  let db = mongoose.connection;
-  db.on('error', console.error.bind(console, 'erro de conexão'));
+  const db = mongoose.connection
+  db.on('error', console.error.bind(console, 'erro de conexão'))
   db.once('open', function() {
-    console.log('Conexão com o banco feita com sucesso');
-  });
+    console.log('Conexão com o banco feita com sucesso')
+  })
 }
 
 module.exports = { connect }
